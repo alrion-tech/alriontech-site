9fbc40904fe46ded79439b55b6e7e7234c66e4ce
Deglier S Amorim <degliersa@gmail.com>
2019-09-14 09:51:53 -0300

    Inserido variáveis de cores do site

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -21,7 +21,17 @@ function Layout({ children }) {
   const GlobalStyle = createGlobalStyle`
 
   @import url(${style});
+  body {
+    font: Roboto 16px / 22px 400;
+    --primary-color: #009BDB;
+    --secondary-color: #85DEF2;
+    --whatsapp-color: #25D366;
+    --light-darker: #575759;
+    --darker: #002626
+    --light: #F2F2F2;
+    --bright: #FFFFFF;
 
+  }
   `
 
   console.log(fundo)
